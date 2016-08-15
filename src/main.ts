db0550370db0bd37f439a1dcbe30f392935ebba9
desingh-rajan <mailme@desinghrajan.in>
2016-08-15 18:59:03 +0530

    Setting up local couchdb as remote couch

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,16 @@ export function configure(aurelia: Aurelia) {
     aurelia.use.basicConfiguration();
     aurelia.start().then(() => aurelia.setRoot('src/iv-man'));
 }
+var db = new PouchDB('ivman');
+db.info().then(function (info) {
+    console.log(info);
+})
+var remoteCouch = 'http://localhost:5984/ivman';
+function sync() {
+    var opts = { live: true };
+    db.replicate.to(remoteCouch, opts);
+    db.replicate.from(remoteCouch, opts);
+}
+if (remoteCouch) {
+    sync();
+}
