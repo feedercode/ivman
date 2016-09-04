bde2ac287715454a87bfb53a78c51cef56fac9ce
desingh-rajan <mailme@desinghrajan.in>
2016-09-04 19:54:29 +0530

    ignoring idea configs

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ export function configure(aurelia: Aurelia) {
     aurelia.use.basicConfiguration();
     aurelia.start().then(() => aurelia.setRoot('src/iv-man'));
 }
+
 var db = new PouchDB('ivman');
 db.info().then(function (info) {
     console.log(info);
-})
+});
+
 var remoteCouch = 'http://localhost:5984/ivman';
 function sync() {
     var opts = { live: true };
